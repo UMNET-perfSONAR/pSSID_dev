c87f90b5ced54432b39d3f73b4ca95c675c2a729
Iffat Saiyara <37493187+Saiyara6133@users.noreply.github.com>
2020-06-19 13:26:45 -0400

    use python3

---
+++ signaltest.py
@@ -0,0 +1,84 @@
+import os
+import signal
+import time
+import argparse
+import random
+import psutil
+
+
+#the arguments are optional
+parser = argparse.ArgumentParser(description='fork')
+parser.add_argument('-t', action='store',
+  help='timelimit')
+parser.add_argument('-w', action='store',
+  help='waitlimit')
+args = parser.parse_args()
+
+
+
+child_exited = False
+
+def list_child():
+    current_process = psutil.Process()
+    children = current_process.children(recursive=True)
+    
+
+    if not children:
+        print("NO CHILDREN")
+        
+    for child in children:
+        print('Child pid is {}'.format(child.pid))
+
+    
+def sigh(signum, frame):
+    global child_exited
+
+    if signum == signal.SIGCHLD:
+        (pid, status) = os.waitpid(-1, 0) 
+        print ("(SIGCHLD) child exited with status: " + str(os.WEXITSTATUS(status)), pid)
+        child_exited = True
+   
+
+
+
+while 1:
+    sleep_time = random.randint(1, 10)
+    pid_child = os.fork()
+    
+    if(pid_child !=0):
+        signal.signal(signal.SIGCHLD, sigh)
+        
+        
+        print("")
+        print("PARENT CHILD: %d %d"% (os.getpid(), pid_child))
+
+        list_child()
+        
+        time.sleep(5)
+
+        if not child_exited:
+          print ("***kill child***", pid_child)
+          os.kill(pid_child, signal.SIGKILL) 
+
+
+        list_child()            
+        child_exited = False
+        continue
+
+    
+    
+    if pid_child == 0:
+        print("CHILD SLEEP: %d %d" % (os.getpid(),sleep_time))
+        time.sleep(sleep_time)
+        exit(0)
+          
+    
+
+
+
+
+
+
+
+
+
